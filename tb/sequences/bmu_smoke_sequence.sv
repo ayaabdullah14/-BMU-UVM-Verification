fe5f62ed7c4f69f1470ba833799da4dde57f831f
ayaabdullah14 <ayaabdullah982@gmail.com>
2026-09-05 04:23:53 +0300

    feat(sequence): add BMU smoke-sequence stub

---
+++ tb/sequences/bmu_smoke_sequence.sv
@@ -0,0 +1,10 @@
+class bmu_smoke_sequence extends bmu_base_sequence;
+  `uvm_object_utils(bmu_smoke_sequence)
+
+  function new(string name = "bmu_smoke_sequence");
+    super.new(name);
+  endfunction
+
+  // TODO: implement initial smoke stimulus.
+endclass
+
