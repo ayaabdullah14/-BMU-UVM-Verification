4187af649fe76e7c313b4f1e26aa8164f1e5a3ab
ayaabdullah14 <ayaabdullah982@gmail.com>
2026-09-05 04:22:28 +0300

    feat(sequencer): add BMU sequencer class stub

---
+++ tb/env/bmu_sequencer.sv
@@ -0,0 +1,8 @@
+class bmu_sequencer extends uvm_sequencer #(bmu_seq_item);
+  `uvm_component_utils(bmu_sequencer)
+
+  function new(string name = "bmu_sequencer", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+endclass
+
