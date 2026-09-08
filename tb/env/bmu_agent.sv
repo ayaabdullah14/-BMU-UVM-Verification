61867c884399086ed75a76a558a09ab7b94105a1
ayaabdullah14 <ayaabdullah982@gmail.com>
2026-09-08 21:44:00 +0300

    implement monitor of BMU

--- tb/env/bmu_agent.sv
+++ tb/env/bmu_agent.sv
@@ -1,10 +1,43 @@
 class bmu_agent extends uvm_agent;
+
   `uvm_component_utils(bmu_agent)
 
-  function new(string name = "bmu_agent", uvm_component parent = null);
+  bmu_driver    driver;
+  bmu_monitor   monitor;
+  bmu_sequencer sequencer;
+
+
+  function new(string name = "bmu_agent",
+               uvm_component parent = null);
     super.new(name, parent);
   endfunction
 
-  // TODO: declare, build, and connect the sequencer, driver, and monitor.
-endclass
 
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+
+    // An active agent also needs a driver and sequencer.
+    if (get_is_active() == UVM_ACTIVE) begin
+      driver = bmu_driver::type_id::create("driver", this);
+      sequencer = bmu_sequencer::type_id::create("sequencer", this);
+       
+    end
+
+    monitor = bmu_monitor::type_id::create("monitor", this);// The monitor exists in both active and passive agents.
+
+
+  endfunction
+
+
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+
+    // Connect the driver to the sequencer in active mode.
+    if (get_is_active() == UVM_ACTIVE) begin
+      driver.seq_item_port.connect(
+        sequencer.seq_item_export);
+    end
+
+  endfunction
+
+endclass
