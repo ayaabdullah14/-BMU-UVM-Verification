268490311a2a953079fdf4a206a29006e9951332
ayaabdullah14 <ayaabdullah982@gmail.com>
2026-09-08 21:44:59 +0300

    rewrite code od sequencer

--- tb/env/bmu_sequencer.sv
+++ tb/env/bmu_sequencer.sv
@@ -1,8 +1,10 @@
+
+//sequencer for bmu sequence items
 class bmu_sequencer extends uvm_sequencer #(bmu_seq_item);
   `uvm_component_utils(bmu_sequencer)
 
   function new(string name = "bmu_sequencer", uvm_component parent = null);
     super.new(name, parent);
   endfunction
-endclass
+endclass :bmu_sequencer
 
